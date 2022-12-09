3e9e373c21362bc1d2febb35390e7eb70ce5b93a
mahmoud-24 <mahmoud.ibrahim2411@gmail.com>
2022-12-09 02:36:31 +0200

    gui

--- GUI/ConnectDB.sh
+++ GUI/ConnectDB.sh
@@ -4,12 +4,25 @@ then
       source ./main.sh
 fi     
 dbname=$(whiptail --title "Connect Database" --inputbox "Please Enter Database You Want To Connect: " 8 40 3>&1 1>&2 2>&3)
-
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+         ...
+      else
+         source ./main.sh
+      fi
 export dbname
 if [[ ! -d ./database/$dbname ]] || [[ -z $dbname ]]
 then  
       whiptail --title "Error" --msgbox "$dbname Database Not Exists Create New " 8 78
-      source ./main.sh
+      exitstatus=$?
+      if [ $exitstatus = 0 ]; then
+         ...
+      else
+         source ./main.sh
+      fi
+      source ./ConnectDB.sh
+      
+
 else
       whiptail --title "Done" --msgbox "Database Exists" 8 78
       source ./Connect-Menu.sh
